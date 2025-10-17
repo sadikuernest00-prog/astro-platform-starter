e2b7f0121b92478ae1d991c2788d8a06ed1837c6
sadikuernest00-prog <sadikuernest00@gmail.com>
2025-10-17 17:38:03 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify/functions';
 
+// If you're using Tailwind, you can include it too:
+// import tailwind from '@astrojs/tailwind';
+
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    // tailwind(), // uncomment if you have Tailwind installed
+  ],
   adapter: netlify(),
 });
