68945af496b4f90bc9a30c41d5f0f08ac94028c2
sadikuernest00-prog <sadikuernest00@gmail.com>
2025-10-17 17:36:49 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
+  integrations: [react()],
   adapter: netlify(),
 });
